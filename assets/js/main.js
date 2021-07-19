c3c34e020cbe323b9c4f21725f96eb41f0a0ef74
DedGuran <dedguran@gmail.com>
2021-07-19 14:26:37 +0800

    add work apm skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,9 +26,38 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
+function toogleSkills() {
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toogleSkills);
+});
 /*==================== QUALIFICATION TABS ====================*/
-
+const tabs = document.querySelectorAll("[data-target]"),
+  tabsContents = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    console.log("test");
+    const target = document.querySelector(tab.dataset.target);
+    tabsContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
 /*==================== SERVICES MODAL ====================*/
 
 /*==================== PORTFOLIO SWIPER  ====================*/
